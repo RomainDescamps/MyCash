11eb15af515f6aae478322012974c81cd1a7a547
Romain Descamps <r.descamps.pro@gmail.com>
2024-11-16 19:01:07 +0100

    Add module

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <h1>Hello World, Jenkins build this</h1>
+    <VitePwaManifest />
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["nuxt-mongoose", "vuetify-nuxt-module", "@vite-pwa/nuxt"],
+  mongoose: {
+    uri: 'mongodb://romain-descamps.freeboxos.fr:27017/MyCash',
+    options: {},
+    modelsDir: 'models',
+    devtools: true,
+  },
+});
